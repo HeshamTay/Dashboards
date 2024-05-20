e948474c6919a7b6d230421f01b5845e5cabce81
HeshamTay <137985061+HeshamTay@users.noreply.github.com>
2024-05-20 18:13:02 +0400

    Update World.py

--- World.py
+++ World.py
@@ -33,6 +33,6 @@ final_df = filtered_df[filtered_df['Year'] == selected_year]
 
 if selected_measurement:
    fig = px.bar(final_df, x='Country Name', y=selected_measurement[0], height=700, width=700)
-   fig.update_layout(yaxis=dict(range=[0, filtered_df['value'].max()]))  # Adjust range to start from zero
+   st.plotly_chart(fig, use_container=True)
 else:
     st.write("Please select at least one measurement.")
